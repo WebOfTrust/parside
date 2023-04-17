0af20afd07d0914cd15d36662d0cf8d0a9957246
Artemkaaas <artem.ivanov@dsr-corporation.com>
2023-04-17 17:40:17 +0300

    Updated tests for cesr group

--- src/message/groups/mod.rs
+++ src/message/groups/mod.rs
@@ -132,9 +132,8 @@ pub mod tests {
     #[test]
     pub fn test_parse_trans_idx_sig_groups() {
         let stream = br#"-FABEFhg5my9DuMU6gw1CVk6QgkmZKBttWSXDzVzWVmxh0_K0AAAAAAAAAAAAAAAAAAAAAAAEFhg5my9DuMU6gw1CVk6QgkmZKBttWSXDzVzWVmxh0_K-AABAADghKct9eYTuSgSd5wdPSYG06tGX7ZRp_BDnrgbSxJpsJtrA-fP7Pa1W602gHeMrO6HZsD1z3tWV5jGlApFmVIB"#;
-
-        let (_rest, group) = CesrGroup::from_stream_bytes(stream).unwrap();
-
+        let (rest, group) = CesrGroup::from_stream_bytes(stream).unwrap();
+        assert!(rest.is_empty());
         match group {
             CesrGroup::TransIdxSigGroupsVariant { value: group } => {
                 assert_eq!(1, group.value.len());
@@ -150,7 +149,6 @@ pub mod tests {
     pub fn test_parse_controller_idx_sigs() {
         let stream = br#"-AABAABg3q8uNg1A2jhEAdbKGf-QupQhNnmZQx3zIyPLWBe6qqLT5ynytivf9EwJhxyhy87a0x2cezDdil4SsM2xxs0O"#;
         let (rest, group) = CesrGroup::from_stream_bytes(stream).unwrap();
-
         assert!(rest.is_empty());
         match group {
             CesrGroup::ControllerIdxSigsVariant { value: group } => {
@@ -164,9 +162,8 @@ pub mod tests {
     #[test]
     pub fn test_parse_non_trans_receipt_couples() {
         let stream = br#"-CABBD8-gMSJ6K1PQ7_gG5ZJn2NkHQJgdkiNrTBz_FWWS_cC0BDc1i44ZX0jaIHh5oNDx-TITbPnI6VEn2nKlqPwkkTF452X7XxYh80tolDpReYwZpnD8TF4Or2v3CpSCikyt6EG"#;
-
-        let (_rest, group) = CesrGroup::from_stream_bytes(stream).unwrap();
-
+        let (rest, group) = CesrGroup::from_stream_bytes(stream).unwrap();
+        assert!(rest.is_empty());
         match group {
             CesrGroup::NonTransReceiptCouplesVariant { value: group } => {
                 assert_eq!(1, group.value.len());
@@ -179,16 +176,25 @@ pub mod tests {
     #[test]
     pub fn test_parse_attached_material_quadlets() {
         let stream = br#"-VA--AABAAAEmCc25ETG2m1Ya-tPGuEqsPywOtusQwXKy076ve56IHXzX2bs0xsdQ4dk0XsanstpThg71ynIy-yUDSue6jMD-BABAABfvC7zCIVOVMol9C4AlSALS9JhL8PCdfgRnJgkXG4U11gFyZbsI_J828POrtwtoOmFhs20hoH1pYw4NZr2cdwN-EAB0AAAAAAAAAAAAAAAAAAAAAAE1AAG2023-02-07T15c00c00d025640p00c00"#;
+        let (rest, message) = CesrGroup::from_stream_bytes(stream).unwrap();
+        assert!(rest.is_empty());
+        match message {
+            CesrGroup::AttachedMaterialQuadletsVariant { value: group } => {
+                assert_eq!(3, group.value.len());
+                assert!(matches!(group.value.get(0).cloned().unwrap(), CesrGroup::ControllerIdxSigsVariant { .. }));
+                assert!(matches!(group.value.get(1).cloned().unwrap(), CesrGroup::WitnessIdxSigsVariant { .. }));
+                assert!(matches!(group.value.get(2).cloned().unwrap(), CesrGroup::FirstSeenReplayCouplesVariant { .. }));
 
-        let (_rest, _message) = CesrGroup::from_stream_bytes(stream).unwrap();
+            }
+            _ => panic!("Unexpected case"),
+        }
     }
 
     #[test]
     pub fn test_parse_trans_last_idx_sig_groups() {
         let stream = br#"-HABEB1f36VmoizOIpBIBv3X4ZiWJQWjtKJ7TMmsZltT0B32-AABAAAKB9u6wyLS9kl_iGVGCqrs-3XqFbyGeOKuiOEA9JZpxI9GMv0GJv2wbY1-sOD_HOJcvXO7LSO8g8MSeRXjtL4I"#;
-
-        let (_rest, group) = CesrGroup::from_stream_bytes(stream).unwrap();
-
+        let (rest, group) = CesrGroup::from_stream_bytes(stream).unwrap();
+        assert!(rest.is_empty());
         match group {
             CesrGroup::TransLastIdxSigGroupsVariant { value: group } => {
                 assert_eq!(1, group.value.len());
@@ -196,5 +202,12 @@ pub mod tests {
             }
             _ => panic!("Unexpected case"),
         }
+    }
+
+    #[test]
+    pub fn test_parse_when_there_is_enough_group_bytes() {
+        let stream = br#"-FABEFhg5my9DuMU6gw1CVk6QgkmZKBttWS"#;
+        let err = CesrGroup::from_stream_bytes(stream).unwrap_err();
+        assert!(matches!(err, ParsideError::StreamDeserializationError(..)));
     }
 }
